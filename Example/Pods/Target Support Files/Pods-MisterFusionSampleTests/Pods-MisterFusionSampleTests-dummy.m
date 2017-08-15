bd2b028002f9744136006e345406ab007eb57427
marty-suzuki <s1180183@gmail.com>
2017-08-15 15:24:29 +0900

    fix example

--- Example/Pods/Target Support Files/Pods-MisterFusionSampleTests/Pods-MisterFusionSampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MisterFusionSampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_MisterFusionSampleTests
-@end
--- Example/Pods/Target Support Files/Pods-MisterFusionSampleTests/Pods-MisterFusionSampleTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_MisterFusionSampleTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MisterFusionSampleTestsVersionString[];
-
